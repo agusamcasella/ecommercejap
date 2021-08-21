6d29d804219cbb0f4195d0c0be824bf9ad36c0b6
agusamcasella <aagusamcasella@gmail.com>
2021-08-21 15:42:09 -0300

    Chequeo formulario con js
    Se cambia el chequeo del formulario par ahacerlo con js, en vez de html.

--- js/login.js
+++ js/login.js
@@ -1,3 +1,29 @@
+function chequearFormulario() {
+    let pass = document.getElementById('pass').value;
+    let email = document.getElementById('email').value;
+    let msjemail = document.getElementById('msjemail');
+    let msjpass = document.getElementById('msjpass');
+
+    if (email === "" || pass === "") {
+        if (email === "") {
+            msjemail.innerHTML = "**Campo Vacìo.";
+        } else {
+            msjemail.innerHTML = " ";
+        }
+        
+        if (pass === "") {
+            msjpass.innerHTML = "**Campo Vacìo.";
+        } else {
+            msjpass.innerHTML = " ";
+        }
+
+        return false;
+        
+    } else {
+        return true;
+    }
+}
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
@@ -5,12 +31,14 @@ document.addEventListener("DOMContentLoaded", function (e) {
     let formulario = document.getElementById('formulario');
     formulario.addEventListener('submit', function (evento) {
         evento.preventDefault();
-        let email = document.getElementById('email').value;
-        localStorage.setItem('email', email);
-        
-        localStorage.setItem('sesion',true);
-        window.location.href = 'index.html';
-        
-       
+
+        if (chequearFormulario()) {
+            let email = document.getElementById('email').value;
+            localStorage.setItem('email', email);
+            localStorage.setItem('sesion', true);
+
+            window.location.href = 'index.html';
+        }
+
     });
 });
